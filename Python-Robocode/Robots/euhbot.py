8c192d9a5e42b7279aab35f6bc1ca55d62776332
matheusfs666 <gokou.ruri42@gmail.com>
2018-12-08 16:17:09 -0200

    initial commit

---
+++ Python-Robocode/Robots/euhbot.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+from robot import Robot #Import a base Robot
+
+
+class EuhBot (Robot): 
+    def init (self):
+        self.setColor(255, 36, 0)
+        self.setGunColor(255, 215, 0)
+        self.setRadarColor(255, 36, 0)
+        self.setBulletsColor(255, 36, 0)
+        
+        size = self.getMapSize() 
+        self.radarVisible(True) 
+        self.lockRadar("gun")
+        
+    def run (self):
+        self.gunTurn(5)
+        pass
+    
+    def sensors (self): 
+        pos = self.getPosition() 
+        x = pos.x() 
+        y = pos.y() 
+        
+        angle = self.getGunHeading() 
+        angle = self.getHeading() 
+        angle = self.getRadarHeading() 
+        list = self.getEnemiesLeft() 
+        for robot in list:
+            id = robot["id"]
+            name = robot["name"]
+        
+    def onHitByRobot (self, robotId, robotName):
+        self.rPrint("damn a bot collided me!")
+
+    def onHitWall (self):
+        pass
+    
+    def onRobotHit (self, robotId, robotName): 
+        self.rPrint('collision with:' + str(robotName)) 
+       
+    def onHitByBullet (self, bulletBotId, bulletBotName, bulletPower): 
+        self.reset()  
+        self.rPrint("hit by " + str(bulletBotName) + "with power:" +
+                     str(bulletPower))
+        
+    def onBulletHit (self, botId, bulletId):
+        self.rPrint("fire done on " +str( botId))
+                    
+    def onBulletMiss (self, bulletId):
+        self.rPrint("the bullet "+ str(bulletId) + " fail")
+        
+    def onRobotDeath (self):
+        self.rPrint("damn I'm Dead")
+    
+    def onTargetSpotted (self, botId, botName, botPos):
+        if (self.enemyp(botName)):
+            self.fire(10)
+
+        self.rPrint("I see the bot:" + str(botId) + "on position: x:"
+                    + str(botPos.x()) + " , y:" + str(botPos.y()))
+
+    # tells whether botName an enemy
+    def enemyp (self, botName):
+        botName == "EuhBot"
+    
